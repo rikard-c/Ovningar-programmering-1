123609e7469c82c352af09771d2f7495dc8f3316
RikardCarlsson <RikardCarlsson@W11-4W8GJS3>
2025-11-21 10:04:28 +0100

    Traffic lights

---
+++ Traffic light/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Xml.Schema;
+
+namespace Traffic_light
+{
+    internal class Program
+    {
+        enum TrafficLight
+        {
+            Red,
+            Yellow,
+            Green        
+        }
+
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+
+            Console.CursorVisible = false;
+            Console.Clear();
+
+            // Draw traffic light
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("         ╔═╗");
+            Console.WriteLine("         ║ ║");
+            Console.WriteLine("         ╠═╣");
+            Console.WriteLine("         ║ ║");
+            Console.WriteLine("         ╠═╣");
+            Console.WriteLine("         ║ ║");
+            Console.WriteLine("         ╚╦╝");
+            Console.WriteLine("          ║");
+            Console.WriteLine("          ║");
+            Console.WriteLine("          ║");
+            Console.WriteLine("          ║");
+            Console.WriteLine("          ║");
+            Console.WriteLine("          ║");
+            Console.WriteLine("          ║");
+            Console.WriteLine("          ║");
+            Console.WriteLine("          ║");
+            Console.WriteLine("          ║");
+
+            TrafficLight current = TrafficLight.Red;
+            // Variabel för att veta när det varit gult för andra gången
+            int restart = 0;
+            
+            while (true) 
+            {
+                if (restart == 2) 
+                { 
+                    current = TrafficLight.Red;
+                    restart = 0;    
+                }
+
+
+                switch (current) 
+                { 
+                    case TrafficLight.Red:
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.SetCursorPosition(10, 4);
+                    Console.Write(" ");
+                    Console.SetCursorPosition(10, 6);
+                    Console.Write(" ");
+                    Console.SetCursorPosition(10, 8);
+                    Console.Write(" ");
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.SetCursorPosition(10, 4);
+                    Console.Write(" ");
+                    System.Threading.Thread.Sleep(1000);
+                    current = TrafficLight.Yellow;
+                    break;
+
+                    case TrafficLight.Yellow:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.SetCursorPosition(10, 6);
+                    Console.Write(" ");
+                    System.Threading.Thread.Sleep(1000);
+                    current = TrafficLight.Green;
+                    restart++;
+                    break;
+
+                    case TrafficLight.Green:
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.SetCursorPosition(10, 4);
+                    Console.Write(" ");
+                    Console.SetCursorPosition(10, 6);
+                    Console.Write(" ");
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.SetCursorPosition(10, 8);
+                    Console.Write(" ");
+                    System.Threading.Thread.Sleep(1000);
+                    current = TrafficLight.Yellow;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Övning 8.6 Traffic light 2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Resources;
+
+namespace Övning_8._6_Traffic_light_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+
+            Console.CursorVisible = false;
+            Console.Clear();
+
+            // Draw traffic light
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("         ╔═╗");
+            Console.WriteLine("         ║ ║");
+            Console.WriteLine("         ╠═╣");
+            Console.WriteLine("         ║ ║");
+            Console.WriteLine("         ╠═╣");
+            Console.WriteLine("         ║ ║");
+            Console.WriteLine("         ╚╦╝");
+            Console.WriteLine("          ║");
+            Console.WriteLine("          ║");
+            Console.WriteLine("          ║");
+            Console.WriteLine("          ║");
+            Console.WriteLine("          ║");
+            Console.WriteLine("          ║");
+            Console.WriteLine("          ║");
+            Console.WriteLine("          ║");
+            Console.WriteLine("          ║");
+            Console.WriteLine("          ║");
+
+            int state = 0;
+            // lookup-tabell för att ha fyra states
+            int[] netxtState = { 1, 2, 3, 0 };
+
+            while (true)
+            {
+                switch (state)
+                {
+                    // Red
+                    case 0:
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.SetCursorPosition(10, 4);
+                        Console.Write(" ");
+                        Console.SetCursorPosition(10, 6);
+                        Console.Write(" ");
+                        Console.SetCursorPosition(10, 8);
+                        Console.Write(" ");
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.SetCursorPosition(10, 4);
+                        Console.Write(" ");
+                        System.Threading.Thread.Sleep(2000);
+                        //state = 1;
+                        break;
+
+                    // Yellow
+                    case 1:
+                    case 3:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.SetCursorPosition(10, 6);
+                        Console.Write(" ");
+                        System.Threading.Thread.Sleep(2000);
+                        //state = 2;
+                        break;
+
+                    // Green
+                    case 2:
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.SetCursorPosition(10, 4);
+                        Console.Write(" ");
+                        Console.SetCursorPosition(10, 6);
+                        Console.Write(" ");
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.SetCursorPosition(10, 8);
+                        Console.Write(" ");
+                        System.Threading.Thread.Sleep(2000);
+                        //state = 3;
+                        break;
+                }
+                // Till nästa state
+                state = netxtState[state];
+            }
+        }
+    }
+}
